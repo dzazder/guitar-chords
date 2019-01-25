1bd2b92f0cd53bb26c6d336a92efb435a9a6dc3f
Bartosz Lampart <dzazder@gmail.com>
2019-01-25 22:06:40 +0100

    added some achords

--- src/models/chords.js
+++ src/models/chords.js
@@ -1,26 +1,90 @@
 var CHORDS = [
     {
-        "id": 1,
+        "id": 11,
         "name": "C-dur",
         "holds": [ 0, 1, 0, 2, 3, 3]
     },
     {
-        "id": 2,
+        "id": 12,
+        "name": "C-dur7",
+        "holds": [ 0, 1, 3, 2, 3, 0]
+    },
+    {
+        "id": 13,
+        "name": "C-moll7",
+        "holds": [ 0, 4, 0, 5, 0, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 14,
+        "name": "C-dur6",
+        "holds": [ 0, 1, 2, 2, 3, 0]
+    },
+    {
+        "id": 15,
+        "name": "C-sus2",
+        "holds": [ 0, 0, 5, 5, 0, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 16,
+        "name": "C-sus4",
+        "holds": [ 0, 6, 5, 5, 0, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 17,
+        "name": "C-9",
+        "holds": [ 3, 3, 0, 2, 3, 0]
+    },
+
+    {
+        "id": 21,
         "name": "G-dur",
         "holds": [ 3, 0, 0, 0, 2, 3]
     },
     {
-        "id": 3,
+        "id": 22,
+        "name": "G-moll",
+        "holds": [ 0, 0, 0, 5, 5, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 23,
+        "name": "G-dur7",
+        "holds": [ 1, 0, 0, 0, 2, 3]
+    },
+    {
+        "id": 24,
+        "name": "G-moll7",
+        "holds": [ 0, 0, 0, 0, 5, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 25,
+        "name": "G-dur6",
+        "holds": [ 0, 3, 0, 0, 2, 3]
+    },
+    {
+        "id": 26,
+        "name": "G-moll6",
+        "holds": [ 0, 5, 0, 5, 5, 0],
+        "fullTresh": 3
+    },
+    {
+        "id": 27,
+        "name": "G-sus4",
+        "holds": [ 0, 0, 5, 5, 5, 0],
+        "fullTresh": 3
+    },
+
+    {
+        "id": 31,
         "name": "D-dur",
         "holds": [ 2, 3, 2, 0, 0, 0],
         "noTouch": [ 6 ]
-    },
-    {
-        "id": 21,
-        "name": "C-moll",
-        "holds": [ 0, 4, 5, 5, 0, 0],
-        "fullTresh": 3
     }
+    
 ];
 
 export default CHORDS;
